e420975ed413d8125fe9e770e225007104f63635
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2021-05-06 15:01:10 +0530

    Home Screen Created

--- Ecommerce/frontend/src/App.js
+++ Ecommerce/frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+import {Container} from "react-bootstrap";
+import Home from "./component/Home/Home";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import ProductScreen from "./component/Productscreen/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <Header/>
+        <main className="py-3">
+            <Container>
+                <Route path='/' component={Home} exact/>
+                <Route path='/product:id' component={ProductScreen} />
+            </Container>
+        </main>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ Ecommerce/frontend/src/component/Productscreen/ProductScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import products from '../../products'
+import {Link} from "react-router-dom";
+function ProductScreen() {
+    // const product = products.find((p) => p._id == match.params.id)
+    return (
+        <>
+            Product
+            {/*<div>*/}
+
+            {/*    <Link tp='/' className='btn btn-light my-3'>GO Back</Link>*/}
+            {/*    {product.name}*/}
+            {/*</div>*/}
+        </>
+    );
+}
+
+export default ProductScreen;
